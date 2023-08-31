e4fd546f95f466650c99889eac73adc8e109a159
lee00286 <crocklee1234@gmail.com>
2023-08-31 15:07:45 -0400

    feat: add stack navigation to navigate between screens

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,55 +1,20 @@
 import React from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import ColorBox from "./components/ColorBox";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import ColorPalette from "./screens/ColorPalette";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <FlatList
-        style={styles.container}
-        data={COLORS}
-        keyExtractor={(item) => item.hexCode}
-        renderItem={({ item }) => (
-          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-        )}
-        ListHeaderComponent={<Text style={styles.titleText}>Solarized</Text>}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ColorPalette" component={ColorPalette} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  titleText: {
-    marginBottom: 10,
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  container: {
-    paddingTop: 40,
-    paddingHorizontal: 10,
-  },
-  safeArea: {
-    flex: 2,
-  },
-});
 
 export default App;
